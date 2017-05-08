850d18d27f2c0a7a3176d07e6f9d0d5f91bfc29a
Manchineella <G.Manchineella@corp.trizetto.com>
2017-05-08 13:07:19 -0400

    formating

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -9,22 +9,22 @@ import { ConfigService } from "app/core/services/config.service";
 @Injectable()
 export class ApiService {
 
-  constructor(private http: Http, private config:ConfigService) {
-  }
-  getLatestStockPrice(Tickers: string[]): Observable<Array<StockInfo>> {
-      return this.http.get(ConfigService.FIN_URL + Tickers.concat(',')) //+"&callback="
-      .map((response: Response) => JSON.parse(response.text().replace("//", "")));
-  }
-   GetJsonPResponse(Tickers: string[], callback) {    
-        var apiServicePath =ConfigService.FIN_URL + Tickers.concat(',');    
-        $.ajax({    
-            crossDomain: true,    
-            dataType: "jsonp",    
-            url: apiServicePath,    
-            async: false,    
-            context: document.body    
-        }).done(function (data) {    
-            callback(data);    
-        });    
-    };    
+    constructor(private http: Http, private config: ConfigService) {
+    }
+    getLatestStockPrice(Tickers: string[]): Observable<Array<StockInfo>> {
+        return this.http.get(ConfigService.FIN_URL + Tickers.concat(',')) //+"&callback="
+            .map((response: Response) => JSON.parse(response.text().replace("//", "")));
+    }
+    GetJsonPResponse(Tickers: string[], callback) {
+        var apiServicePath = ConfigService.FIN_URL + Tickers.concat(',');
+        $.ajax({
+            crossDomain: true,
+            dataType: "jsonp",
+            url: apiServicePath,
+            async: false,
+            context: document.body
+        }).done(function (data) {
+            callback(data);
+        });
+    };
 }
